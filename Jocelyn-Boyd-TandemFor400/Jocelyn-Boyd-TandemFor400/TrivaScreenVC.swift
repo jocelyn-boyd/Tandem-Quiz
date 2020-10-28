74e03bf4f7e78dd296a18f2edfbfacbe4b72388a
Jocelyn <jocelynboyd@pursuit.org>
2020-10-27 23:23:12 -0400

    Setup Trivia Screen via Storyboard and IBOutlets in VC

---
+++ Jocelyn-Boyd-TandemFor400/Jocelyn-Boyd-TandemFor400/TrivaScreenVC.swift
@@ -0,0 +1,28 @@
+//
+//  TrivaScreenVC.swift
+//  Jocelyn-Boyd-TandemFor400
+//
+//  Created by Jocelyn Boyd on 10/27/20.
+//
+
+import UIKit
+
+class TrivaScreenVC: UIViewController {
+
+    @IBOutlet var triviaProgressBar: UIProgressView!
+    @IBOutlet var questionLabel: UILabel!
+    
+    @IBOutlet var optionOne: UIButton!
+    @IBOutlet var optionTwo: UIButton!
+    @IBOutlet var optionThree: UIButton!
+    @IBOutlet var optionFour: UIButton!
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+}
