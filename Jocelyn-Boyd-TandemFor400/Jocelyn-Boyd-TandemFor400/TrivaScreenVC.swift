8a49420c5c98300ec6d7a2c2a6cfd3325e59a713
Jocelyn <jocelynboyd@pursuit.org>
2020-10-28 22:59:14 -0400

    Update quiz round to ten questions

--- Jocelyn-Boyd-TandemFor400/Jocelyn-Boyd-TandemFor400/TrivaScreenVC.swift
+++ Jocelyn-Boyd-TandemFor400/Jocelyn-Boyd-TandemFor400/TrivaScreenVC.swift
@@ -29,11 +29,6 @@ class TrivaScreenVC: UIViewController {
         setQuestion()
     }
     
-    override func viewWillAppear(_ animated: Bool) {
-        super.viewWillAppear(true)
-        loadTriviaDataFromJSON()
-    }
-    
     @IBAction private func answerButtonPressed(_ sender: UIButton) {
         let question = triviaInfo?[currentQuestionIndex - 1]
         if sender.titleLabel?.text == question?.correct {
