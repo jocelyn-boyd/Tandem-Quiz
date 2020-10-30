5b9fcd765a8d3f0a2248c1a7793b5a27afbca3bb
Jocelyn <jocelynboyd@pursuit.org>
2020-10-30 00:03:45 -0400

    Renamed TriviaScreenVC -> TriviaViewController

--- Jocelyn-Boyd-TandemFor400/Jocelyn-Boyd-TandemFor400/TrivaViewController.swift
+++ Jocelyn-Boyd-TandemFor400/Jocelyn-Boyd-TandemFor400/TrivaViewController.swift
@@ -6,7 +6,7 @@
 //
 import UIKit
 
-class TrivaScreenVC: UIViewController {
+class TrivaViewController: UIViewController {
     
     // MARK: - Outlets
     @IBOutlet private var triviaProgressBar: UIProgressView!
